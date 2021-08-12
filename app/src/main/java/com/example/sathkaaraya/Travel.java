42a099d43b521a8adfefda224b1248ddb3ef01d1
Chamod-G <IT20409982@my.sliit.lk>
2021-08-12 23:46:51 +0530

    travel

---
+++ app/src/main/java/com/example/sathkaaraya/Travel.java
@@ -0,0 +1,14 @@
+package com.example.sathkaaraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Travel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel);
+    }
+}
