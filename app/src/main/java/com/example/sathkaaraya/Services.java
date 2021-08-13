b404eaefdb36a1f51125dcf91c524a718e6da5c0
danushka9999 <dranasinghe99@gmail.com>
2021-08-13 22:05:00 +0530

    App top bar implementation

--- app/src/main/java/com/example/sathkaaraya/Services.java
+++ app/src/main/java/com/example/sathkaaraya/Services.java
@@ -1,14 +1,17 @@
 package com.example.sathkaaraya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Services extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+
     }
 }
