6dbc959f6b099f90304d09de954bb3c22c6bdddd
di-nethra <anjana.dinethra@gmail.com>
2021-08-13 13:47:15 +0530

    userLandingpage started

--- app/src/main/java/com/example/sathkaaraya/Services.java
+++ app/src/main/java/com/example/sathkaaraya/Services.java
@@ -2,13 +2,24 @@ package com.example.sathkaaraya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Services extends AppCompatActivity {
-
+    private static int ScrenOnTime = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Services.this,userLanding.class);
+                startActivity(intent);
+                finish();
+            }
+        },ScrenOnTime);
     }
 }
