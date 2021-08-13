9df809dfb6d7a5c17af85f4773a34fe9d380a5c9
di-nethra <anjana.dinethra@gmail.com>
2021-08-13 22:22:57 +0530

    loginPage added

--- app/src/main/java/com/example/sathkaaraya/RegistrationPage.java
+++ app/src/main/java/com/example/sathkaaraya/RegistrationPage.java
@@ -2,13 +2,15 @@ package com.example.sathkaaraya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class RegistrationPage extends AppCompatActivity {
 
-
+    private static int ScrenOnTime = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,14 @@ public class RegistrationPage extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.countryCodes));
         countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         countryCodeSpinner.setAdapter(countryCodeAdapter);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(RegistrationPage.this,LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        },ScrenOnTime);
 
     }
 }
