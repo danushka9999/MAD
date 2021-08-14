d9184a400362730a72646b86e4951408ce66c0f0
Chamod-G <IT20409982@my.sliit.lk>
2021-08-14 11:23:14 +0530

    string values

---
+++ app/src/main/java/com/example/sathkaaraya/WhereTo.java
@@ -0,0 +1,14 @@
+package com.example.sathkaaraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhereTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where_to);
+    }
+}
