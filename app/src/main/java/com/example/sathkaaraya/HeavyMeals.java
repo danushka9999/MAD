21a572c7ce43b11640adc5b588e68553923cc55c
Yomal Bandara <yomalb07@gmail.com>
2021-08-14 15:50:22 +0530

    HeavyMeals Form and UI

---
+++ app/src/main/java/com/example/sathkaaraya/HeavyMeals.java
@@ -0,0 +1,14 @@
+package com.example.sathkaaraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeavyMeals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heavy_meals);
+    }
+}
