129d111df3f9e69dade563840a5cc79a0dff5e6e
Chamod-G <IT20409982@my.sliit.lk>
2021-09-21 16:50:50 +0530

    details form completed

---
+++ app/src/main/java/com/example/sathkaaraya/Form.java
@@ -0,0 +1,61 @@
+package com.example.sathkaaraya;
+
+public class Form {
+    private String name;
+    private Integer roomNo;
+    private String startingDate;
+    private String endingDate;
+    private Integer noOfAdults;
+    private Integer noOfChildren;
+
+    public Form() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(Integer roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public String getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(String startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public String getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate(String endingDate) {
+        this.endingDate = endingDate;
+    }
+
+    public Integer getNoOfAdults() {
+        return noOfAdults;
+    }
+
+    public void setNoOfAdults(Integer noOfAdults) {
+        this.noOfAdults = noOfAdults;
+    }
+
+    public Integer getNoOfChildren() {
+        return noOfChildren;
+    }
+
+    public void setNoOfChildren(Integer noOfChildren) {
+        this.noOfChildren = noOfChildren;
+    }
+}
