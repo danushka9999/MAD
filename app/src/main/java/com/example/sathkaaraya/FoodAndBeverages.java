458a57f1e81fa5985927ef279454deb104b9c95c
Yomal Bandara <yomalb07@gmail.com>
2021-09-21 12:29:26 +0530

    Heavy Meal UI implemented

--- app/src/main/java/com/example/sathkaaraya/FoodAndBeverages.java
+++ app/src/main/java/com/example/sathkaaraya/FoodAndBeverages.java
@@ -1,14 +1,36 @@
 package com.example.sathkaaraya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class FoodAndBeverages extends AppCompatActivity {
+public class FoodAndBeverages extends AppCompatActivity implements View.OnClickListener {
+    CardView cardView_heavyMeals;
+    CardView cardView_snacks;
+    CardView cardView_beverages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_and_beverages);
+
+        cardView_heavyMeals = findViewById(R.id.cv_heavyMeals);
+        cardView_heavyMeals.setOnClickListener(this);
+
+        cardView_snacks = findViewById(R.id.cv_snacks);
+        cardView_beverages = findViewById(R.id.cv_beverages);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cv_heavyMeals:
+                startActivity(new Intent(this,HeavyMeals.class));
+                break;
+        }
+
     }
 }
