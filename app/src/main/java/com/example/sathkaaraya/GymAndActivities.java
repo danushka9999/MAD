4fa713937cfb5ac7c96b4ca502304a9ad843e605
danushka9999 <dranasinghe99@gmail.com>
2021-09-22 21:04:03 +0530

    Gym and Activities main implemented

---
+++ app/src/main/java/com/example/sathkaaraya/GymAndActivities.java
@@ -0,0 +1,14 @@
+package com.example.sathkaaraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GymAndActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gym_and_activities);
+    }
+}
