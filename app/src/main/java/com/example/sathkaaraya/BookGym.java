6f7b406abc0c50ecf6f590e1732098f024aa0168
danushka9999 <dranasinghe99@gmail.com>
2021-09-23 21:02:29 +0530

    Time picker implemented

--- app/src/main/java/com/example/sathkaaraya/BookGym.java
+++ app/src/main/java/com/example/sathkaaraya/BookGym.java
@@ -1,5 +1,6 @@
 package com.example.sathkaaraya;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.DateValidatorPointForward;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.timepicker.MaterialTimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -24,7 +28,9 @@ public class BookGym extends Fragment {
 
 
     private Button mDatePickerBtn;
+    private Button mTimePicker;
     private TextView mSelectedDateText;
+    private TextView mSelectedTimeText;
 
     public BookGym() {
         // Required empty public constructor
@@ -39,7 +45,9 @@ public class BookGym extends Fragment {
 
         View view =  inflater.inflate(R.layout.fragment_book_gym, container, false);
         mDatePickerBtn = view.findViewById(R.id.date_picker_btn);
+        mTimePicker = view.findViewById(R.id.time_picker_button);
         mSelectedDateText = view.findViewById(R.id.selected_date);
+        mSelectedTimeText = view.findViewById(R.id.selected_time);
 
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         calendar.clear();
@@ -78,6 +86,29 @@ public class BookGym extends Fragment {
             public void onPositiveButtonClick(Object selection) {
                 mSelectedDateText.setText("Selected Date : " + materialDatePicker.getHeaderText());
 
+            }
+        });
+
+        //  Time picker
+        mTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar=Calendar.getInstance();
+                int hours=calendar.get(Calendar.HOUR_OF_DAY);
+                int mins=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog= new TimePickerDialog(getActivity(), R.style.Theme_AppCompat_Light_Dialog, new TimePickerDialog.OnTimeSetListener(){
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar c=Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format= new SimpleDateFormat("k:mm a");
+                        String time = format.format(c.getTime());
+                        mSelectedTimeText.setText("Selected Time : " + time);
+                    }
+                },hours ,mins,false);
+                timePickerDialog.show();
             }
         });
 
